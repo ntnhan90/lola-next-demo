c7c8359872b7aea4a24eeff4d11d510feaf1e82e
nguyễn thái nhân <ntnhan90@gmail.com>
2026-06-15 21:58:26 +0700

    first

---
+++ app/components/cake/CakeCustomizer.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+export default function CakeCustomizer() {
+  return (
+    <div className="container-fluid py-4">
+      <div className="row">
+        <div className="col-lg-8">
+          <div className="preview-panel">
+          Preview
+          </div>
+        </div>
+        <div className="col-lg-4">
+          <div className="sidebar-panel">
+          Sidebar
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
